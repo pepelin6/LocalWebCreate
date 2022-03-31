a9d46301685c6188d3daff0dedf3fa5e17240dc1
Даниил <kourin.daniil@gmail.com>
2022-03-31 11:24:59 +0300

    31.03.2022 диплом Артемия

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp4.viewmodels;
+
+namespace WpfApp4
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            ListD.Items.Add(new DeviceVM(@"Images\Computer.png", "Компьютер" ));
+            ListD.Items.Add(new DeviceVM(@"Images\iconfinder.png", "Ноутбук"));
+            ListD.Items.Add(new DeviceVM(@"Images\router.png", "Роутер"));
+        }
+    }
+}
---
+++ viewmodels/DeviceVM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp4.viewmodels
+{
+    class DeviceVM
+    {
+        public string ImageSource { get; }
+        public string Description {get; }
+        public DeviceVM(string imageSource, string description)
+        {
+            ImageSource = imageSource;
+            Description = description;
+
+        }
+    }
+}
